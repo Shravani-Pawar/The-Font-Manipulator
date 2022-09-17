966b4f976e4949f90bb84969335a392527f22532
Shravani Pawar <89009083+Shravani-Pawar@users.noreply.github.com>
2022-09-17 13:05:27 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,41 @@
+text="Peter";
+
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(550,300);
+    
+    video.position(90,250);
+     console.log("webcam loaded!");
+    canvas= createCanvas(550,300);
+    canvas.position(700,250);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function draw()
+{
+    background('#969A97');
+    textSize(difference);
+    fill('red');
+    text(text,50,400);
+}
+
+
+function gotPoses()
+{
+    if(results.length>0)
+    {
+        console.log(results);
+       
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftwristx="+leftWristX+"rightWristX"+rightWristX+"difference"+difference);
+
+    }
+}
