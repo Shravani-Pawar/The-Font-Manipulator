691330f581dfbaddc1d0a864e51f6a56144f4721
Shravani Pawar <89009083+Shravani-Pawar@users.noreply.github.com>
2022-09-17 16:15:58 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -43,6 +43,6 @@ function draw()
     textSize(size=floor(leftWristX-rightWristX));
     
     document.getElementById("px").innerHTML=size+"px";
-    fill('#AD33FF');
+    fill('#FFFF00');
     text(txt,50,230);
 }
